6f631afaa39af6100f7514a28c819d07d1fee9a3
Pranish Khadge <215408924+Pranish-Khadge@users.noreply.github>
2026-01-18 21:51:50 +0545

    hehehe

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CalenderApp from "./components/CalenderApp";
+import "./components/CalenderApp.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <CalenderApp />
+    </div>
+  );
+};
+
+export default App;
